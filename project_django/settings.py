735da040eba86bb049816a97bbed3f66415d9ea7
mrifqyz <rifqy.lxgw@gmail.com>
2022-08-28 22:14:29 +0700

    add django app example

--- project_django/settings.py
+++ project_django/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'example_app',
 ]
 
 MIDDLEWARE = [
